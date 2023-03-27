e01557359f9f09b85a46336eaeaf36f6a08b5c8e
Galaxies99 <tony.fang.galaxies@gmail.com>
2023-03-27 15:29:32 +0800

    update

--- setup.py
+++ setup.py
@@ -3,28 +3,24 @@ from setuptools import setup, find_packages
 setup(
     name = 'easyrobot',
     version = '0.0.2',
+    license = 'MIT',
     description = 'An easy and unified interface for robot (and grippers, etc.)',
     author = "Hongjie Fang",
     author_email = "tony.fang.galaxies@gmail.com, galaxies@sjtu.edu.cn",
     maintainer = "Hongjie Fang",
     maintainer_email = "tony.fang.galaxies@gmail.com, galaxies@sjtu.edu.cn",
-    package = find_packages(include = [
-        'easyrobot',
-        'easyrobot.robot',
-        'easyrobot.gripper', 
-        'easyrobot.ftsensor',
-        'easyrobot.pedal',
-        'easyrobot.utils',
-        'easyrobot.robot.*',
-        'easyrobot.gripper.*',
-        'easyrobot.ftsensor.*',
-        'easyrobot.pedal.*',
-        'easyrobot.utils.*'
-    ]),
+    packages = find_packages(exclude = ['docs', 'assets']),
     install_requires = [
         'numpy',
         'pyserial',
         'pygame',
         'modbus_tk'
-    ]
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe = False
 )
